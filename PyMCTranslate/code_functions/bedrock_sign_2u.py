aaadcf3b16314bcc5f8830f9e015a0065ede3cc1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-01-06 12:46:16 +0000

    Improve raw text (#68)
    * Add Java raw text parsing
    
    * Refactor and add Java NBT encoding
    
    * Implement Java content writing
    
    * Add section string encoding
    
    * Reformat
    
    * Refactor colour storage
    
    * Refactor colour storage
    
    * Add slots to dataclasses
    
    * Implement section string parsing
    
    * Fix type hints
    
    * Convert colour to name where possible
    
    * Simplify components from section text
    
    * Add tests
    
    * Implement Java JSON text format
    
    * Add test cases
    
    * Fix sign translations
    
    * Fix 1.21.5 sign translations
    
    * Reformat
    
    * Fix NBT paths
    
    * Fix nbt input
    
    * Fix return
    
    * Fix conversion issues
    
    * Fix return
    
    * Add default content
    
    If the game cannot find a content type, it errors and does not render anything
    
    * Bump python version
    
    * Fix tests

--- PyMCTranslate/code_functions/bedrock_sign_2u.py
+++ PyMCTranslate/code_functions/bedrock_sign_2u.py
@@ -1,30 +1,20 @@
 from amulet_nbt import CompoundTag, StringTag
 
-from PyMCTranslate.py3.util.raw_text import section_string_to_raw_text_list
-
 
 def main(nbt):
-    if not isinstance(nbt, CompoundTag):
-        return []
-    text = nbt.get("Text")
-    if not isinstance(text, StringTag):
-        return []
-    out = []
-
-    if text:
-        for line_num, line in enumerate(section_string_to_raw_text_list(text.py_str)):
-            out.append(
+    if isinstance(nbt, CompoundTag):
+        text = nbt.get("Text")
+        if isinstance(text, StringTag):
+            return [
                 [
                     "",
                     "compound",
                     [
                         ("utags", "compound"),
                         ("front_text", "compound"),
-                        ("messages", "list"),
                     ],
-                    line_num,
-                    StringTag(line),
+                    "bedrock_string",
+                    text,
                 ]
-            )
-
-    return out
+            ]
+    return []
--- PyMCTranslate/code_functions/bedrock_sign_2u_120.py
+++ PyMCTranslate/code_functions/bedrock_sign_2u_120.py
@@ -1,22 +1,17 @@
 from amulet_nbt import CompoundTag, StringTag
 
-from PyMCTranslate.py3.util.raw_text import section_string_to_raw_text_list
-
 
 def main(nbt):
-    if not isinstance(nbt, CompoundTag):
-        return []
-
     out = []
-
-    for group_1, group_2 in (("FrontText", "front_text"), ("BackText", "back_text")):
-        text_compound = nbt.get(group_1)
-        if isinstance(text_compound, CompoundTag):
-            text = text_compound.get("Text")
-            if isinstance(text, StringTag):
-                for line_num, line in enumerate(
-                    section_string_to_raw_text_list(text.py_str)
-                ):
+    if isinstance(nbt, CompoundTag):
+        for group_1, group_2 in (
+            ("FrontText", "front_text"),
+            ("BackText", "back_text"),
+        ):
+            text_compound = nbt.get(group_1)
+            if isinstance(text_compound, CompoundTag):
+                text = text_compound.get("Text")
+                if isinstance(text, StringTag):
                     out.append(
                         [
                             "",
@@ -24,11 +19,9 @@ def main(nbt):
                             [
                                 ("utags", "compound"),
                                 (group_2, "compound"),
-                                ("messages", "list"),
                             ],
-                            line_num,
-                            StringTag(line),
+                            "bedrock_string",
+                            text,
                         ]
                     )
-
     return out
